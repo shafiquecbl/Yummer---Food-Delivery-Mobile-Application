3f4084d6e2ca0bd3c805c42ca7539e139a88b23b
Hamza <hamzarauf5498@gmail.com>
2021-09-07 14:54:47 +0500

    Main Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:secure_hops/Screens/Onboarding/OnBoarding.dart';
 
-import 'package:secure_hops/home.dart';
-
 void main() {
   runApp(MyApp());
 }
