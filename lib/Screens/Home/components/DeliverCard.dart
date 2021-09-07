382f3d71b924e3c8ad02173f5efcaa6f2a9d9dae
Hamza <hamzarauf5498@gmail.com>
2021-09-07 18:55:19 +0500

    aa

--- lib/Screens/Home/components/DeliverCard.dart
+++ lib/Screens/Home/components/DeliverCard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:secure_hops/constants.dart';
+
+import 'Categories.dart';
 import 'ItemList.dart';
 import 'OffercardList.dart';
 
@@ -7,29 +9,54 @@ class DeliverCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Column(children: [
-      Column(children: [
+    return Column(
+      children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text('deliver to:',
-                style: TextStyle(
-                    fontSize: 20,
-                    color: kWhiteColor,
-                    fontWeight: FontWeight.w500)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  width: size.width / 1.7,
-                  child: Text(
-                    '8000 S Kirkland Ave, Chicago, IL ...',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(color: kWhiteColor, fontSize: 16),
-                  ),
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                demoAvatar,
+              ),
+              radius: 30,
+            ),
+            Container(
+              height: 80,
+              width: size.width / 1.3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text('deliver to:',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: kWhiteColor,
+                            fontWeight: FontWeight.w500)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: size.width / 1.7,
+                          child: Text(
+                            '8000 S Kirkland Ave, Chicago, IL ...',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(color: kWhiteColor, fontSize: 16),
+                          ),
+                        ),
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.arrow_forward_ios,
+                              color: kWhiteColor,
+                            )),
+                      ],
+                    )
+                  ],
                 ),
-              ],
+              ),
             ),
           ],
         ),
@@ -42,7 +69,7 @@ class DeliverCard extends StatelessWidget {
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
         Expanded(child: ItemList()),
-      ])
-    ]);
+      ],
+    );
   }
 }
