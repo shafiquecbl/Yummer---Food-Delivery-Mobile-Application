3a3eceb4c7960835893c57923744fc6cefd7ed33
Hamza <hamzarauf5498@gmail.com>
2021-09-22 16:44:42 +0500

    updated

--- lib/Screens/Profile/components/changepass.dart
+++ lib/Screens/Profile/components/changepass.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:secure_hops/API/Api_Services/Api_Manager.dart';
+import 'package:secure_hops/Provider/user_provider.dart';
 import 'package:secure_hops/Widgets/AppBar.dart';
 import 'package:secure_hops/Widgets/button.dart';
 import 'package:secure_hops/model/loginResponseModel.dart';
@@ -27,106 +29,103 @@ class _ChangePassState extends State<ChangePass> {
       TextEditingController();
 
   String? usercode;
+  LoginStorage? loginStorage;
 
   @override
   void initState() {
-    getuser().then((value) {
-      usercode = value.userCode.toString();
-    });
+    LoginStorage provider = Provider.of(context, listen: false);
+    provider.getuser();
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kBackgroundColor,
-      appBar: appBar(context, title: 'Change Password'),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 20,
-                ),
-                Center(
-                  child: Stack(
-                    children: [
-                      CircleAvatar(
-                          radius: 60,
-                          backgroundColor: Colors.grey[200],
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(70),
-                            child: Image.network(
-                              demoAvatar,
-                              width: 120,
-                              height: 120,
-                              fit: BoxFit.cover,
-                            ),
-                          )),
-                    ],
+    return Consumer<LoginStorage>(builder: (context, login, child) {
+      loginStorage = login;
+      return Scaffold(
+        backgroundColor: kBackgroundColor,
+        appBar: appBar(context, title: 'Change Password'),
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 20,
                   ),
-                ),
-                box(),
-                TextFormField(
-                    decoration: InputDecoration(
-                      focusedBorder: focusedBorder,
-                      hintText: "Enter old password",
-                      hintStyle: TextStyle(color: Colors.grey),
-                      labelText: "Old Password",
+                  Center(
+                    child: Stack(
+                      children: [
+                        CircleAvatar(
+                            radius: 60,
+                            backgroundColor: Colors.grey[200],
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(70),
+                              child: Image.network(
+                                demoAvatar,
+                                width: 120,
+                                height: 120,
+                                fit: BoxFit.cover,
+                              ),
+                            )),
+                      ],
                     ),
-                    controller: _oldpassTextEditingController,
-                    validator: (value) {}),
-                box(),
-                TextFormField(
-                    decoration: InputDecoration(
-                      focusedBorder: focusedBorder,
-                      hintText: "Enter new password",
-                      hintStyle: TextStyle(color: Colors.grey),
-                      labelText: "New PassWord",
-                    ),
-                    controller: _newpassTextEditingController,
-                    validator: (value) {}),
-                box(),
-                TextFormField(
-                    decoration: InputDecoration(
-                      focusedBorder: focusedBorder,
-                      hintText: "Enter confirm password",
-                      hintStyle: TextStyle(color: Colors.grey),
-                      labelText: "Confirm PassWord",
-                    ),
-                    controller: _cpassTextEditingController,
-                    validator: (value) {}),
-                box(),
-                box(),
-                box(),
-                MyButton(
-                    text: 'SAVE PASSWORD',
-                    onPressed: () {
-                      APIService().changepass(context,
-                          newpass: _newpassTextEditingController.text,
-                          oldpass: _oldpassTextEditingController.text,
-                          usercode: usercode);
-                    })
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+                  ),
+                  box(),
+                  TextFormField(
+                      decoration: InputDecoration(
+                        focusedBorder: focusedBorder,
+                        hintText: "Enter old password",
+                        hintStyle: TextStyle(color: Colors.grey),
+                        labelText: "Old Password",
+                      ),
+                      controller: _oldpassTextEditingController,
+                      validator: (value) {}),
+                  box(),
+                  TextFormField(
+                      decoration: InputDecoration(
+                        focusedBorder: focusedBorder,
+                        hintText: "Enter new password",
+                        hintStyle: TextStyle(color: Colors.grey),
+                        labelText: "New PassWord",
+                      ),
+                      controller: _newpassTextEditingController,
+                      validator: (value) {}),
+                  box(),
+                  TextFormField(
+                      decoration: InputDecoration(
+                        focusedBorder: focusedBorder,
+                        hintText: "Enter confirm password",
+                        hintStyle: TextStyle(color: Colors.grey),
+                        labelText: "Confirm PassWord",
+                      ),
+                      controller: _cpassTextEditingController,
+                      validator: (value) {}),
+                  box(),
+                  box(),
+                  box(),
+                  MyButton(
+                      text: 'SAVE PASSWORD',
+                      onPressed: () {
+                        APIService().changepass(context,
+                            newpass: _newpassTextEditingController.text,
+                            oldpass: _oldpassTextEditingController.text,
+                            usercode: login.loginResponseModel!.userCode);
+                      })
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    });
   }
 
   Widget box() {
     return SizedBox(
       height: 25,
     );
-  }
-
-  Future<LoginResponseModel> getuser() async {
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    String res = pref.getString('Login').toString();
-    var jsonMap = json.decode(res);
-    return LoginResponseModel.fromJson(jsonMap);
   }
 }
